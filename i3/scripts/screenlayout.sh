c2debc673fa58cbbf757a7ee502dd374ca670c1f
ggueli <gianluca.gue99@gmail.com>
2025-05-23 22:07:02 +0200

    feat(i3): add script for monitor setup

---
+++ i3/scripts/screenlayout.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONNECTED_STATUS="connected"
+SETUP_PATH="${HOME}/.screenlayout"
+
+LEFT_MONITOR=$(xrandr | grep DP-3-3 | awk '{print $2}')
+RIGHT_MONITOR=$(xrandr | grep DP-3-1 | awk '{print $2}')
+
+if [ $LEFT_MONITOR = $CONNECTED_STATUS ] && [ $RIGHT_MONITOR = $CONNECTED_STATUS ]; then
+	$SETUP_PATH/home-setup.sh
+else 
+	$SETUP_PATH/away.sh
+fi
+
